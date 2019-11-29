d337ca289b8e7736a3a25f31bcb58ae91b48540c
PinkyUni <30stmars6277@gmail.com>
2019-11-29 16:01:47 +0300

    fixed make message fun

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -17,6 +17,7 @@ abstract class BaseMessage(
         var message_id: Int = 0
 
         fun makeMessage(from: User?, chat: Chat, date: Date, payload: String, type: String, isIncoming:Boolean = false): BaseMessage {
+            message_id++
             return when (type) {
                 "image" -> ImageMessage(message_id.toString(), from, chat, isIncoming, date, payload)
                 else -> TextMessage(message_id.toString(), from, chat, isIncoming, date, payload)
